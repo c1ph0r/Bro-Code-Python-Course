eaede42a6783e951d7005749db8867163c0ba854
Ed Miro <c1ph0r@protonmail.com>
2024-01-27 07:31:07 -0700

    Create Data_Types.py

---
+++ Data_Types.py
@@ -0,0 +1,25 @@
+#Strings
+first_name = "Ed"
+last_name = "Ward"
+full_name = first_name +" "+ last_name
+print("Hello "+full_name)
+print(type(full_name))
+
+#Integers
+age = 21
+age = age + 1
+print(age)
+print(type(age))
+print("Your age is: "+str(age))
+
+#Floats
+height = 250.5
+print(height)
+print(type(height))
+print("Your height is "+str(height)+" cm")
+
+#Booleans
+human = False
+print(human)
+print(type(human))
+print("Are you a human: "+str(human))
