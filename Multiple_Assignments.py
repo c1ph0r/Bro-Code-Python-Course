56f85b06e949c1b8fb1adf862d9f8d4b1abd24d2
Ed Miro <c1ph0r@protonmail.com>
2024-01-27 07:32:11 -0700

    Create Multiple_Assignments.py

---
+++ Multiple_Assignments.py
@@ -0,0 +1,21 @@
+#name = "Ed"
+#age = 21
+#attractive = True
+
+name, age, attractive = "Ed", 21, True
+
+print(name)
+print(age)
+print(attractive)
+
+#Sponge = 30
+#Pat = 30
+#Sandy = 30
+#Squid = 30
+
+Sponge = Pat = Sandy = Squid = 30
+
+print(Sponge)
+print(Pat)
+print(Sandy)
+print(Squid)
