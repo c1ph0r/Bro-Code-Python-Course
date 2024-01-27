96d5da35c1bc7156528ffdf9c4adccc33c1606ab
Ed Miro <c1ph0r@protonmail.com>
2024-01-27 07:39:23 -0700

    Create String_Methods.py

---
+++ String_Methods.py
@@ -0,0 +1,21 @@
+name = "edward"
+#length
+print(len(name))
+#find
+print(name.find("w"))
+#capitalize
+print(name.capitalize())
+#upper
+print(name.upper())
+#lower
+print(name.lower())
+#isdigit?
+print(name.isdigit())
+#isalpha?
+print(name.isalpha())
+#count
+print(name.count("a"))
+#replace
+print(name.replace("e","y"))
+#multiply strings
+print(name*3)
