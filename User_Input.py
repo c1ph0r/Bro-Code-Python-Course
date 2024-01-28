383b88f29145c9d684af04d3b008c50d9e062515
Ed Miro <c1ph0r@protonmail.com>
2024-01-28 07:13:18 -0700

    Create User_Input.py

---
+++ User_Input.py
@@ -0,0 +1,11 @@
+#Accepting user input
+
+name = input("What is your name?: ")
+print("Hello "+ name)
+
+#Type casting
+age = int(input("How old are you?: "))
+age = age + 1
+print("You are "+str(age)+" years old.")
+height = float(input("How tall are you?: "))
+print("You are "+str(height)+" tall.")
